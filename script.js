cc5b003039af6dd1bb31f9bfec61377fc73a3cf1
Lasse Haslund <tofthaslund@gmail.com>
2021-09-05 14:41:58 +0100

    created querySelctors for all needed infomation for app

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let weather = {
     "apiKey": "e1e665850557c078c2708952d16655b5",
-    fetchWeather: function(){
+    fetchWeather: function(city){
         fetch(
-            "api.openweathermap.org/data/2.5/weather?q=Denver&appid=e1e665850557c078c2708952d16655b5"
+            "api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
             )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = Math.trunc(temp) + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = speed.toFixed(1) + " Km/h";
+
     }
 }
